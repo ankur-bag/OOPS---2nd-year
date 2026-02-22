2c459ca8fdc8e3d3bf121113f3e8366c4d37aa0b
Ankur Bag <ankurbag700@gmail.com>
2026-02-22 15:14:32 +0530

    static block

---
+++ staticBlock.java
@@ -0,0 +1,16 @@
+class StaticBlock {
+
+    static int number;
+
+    // Static block
+    static {
+        System.out.println("Static block is executed");
+        number = 100;   // Initializing static variable
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println("Main method is executed");
+        System.out.println("Value of number: " + number);
+    }
+}
